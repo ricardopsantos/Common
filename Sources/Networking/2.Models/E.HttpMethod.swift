5e143cd7f0c6d6274e59096600059b4b5fc7ac30
Ricardo Santos <ricardopsantos@users.noreply.github.com>
2026-03-12 09:16:23 +0000

    Code review

---
+++ Sources/Networking/2.Models/E.HttpMethod.swift
@@ -0,0 +1,18 @@
+//
+//  Created by Ricardo Santos on 01/01/2023.
+//  Copyright © 2024 - 2019 Ricardo Santos. All rights reserved.
+//
+
+import Foundation
+
+// Used on [SimpleNetworkClient] and [NetworkAgent]
+
+public extension CommonNetworking {
+    enum HttpMethod: String {
+        case get = "GET"
+        case post = "POST"
+        case put = "PUT"
+        case delete = "DELETE"
+        case patch = "PATCH"
+    }
+}
